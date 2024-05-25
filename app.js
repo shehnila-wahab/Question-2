2e73a07f38ccfb56238c1627d43e473ee601bcdc
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-05-25 22:23:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+var personName = "shehnila";
+console.log("Hellow ".concat(personName, " would you like to learn some python today?"));
---
+++ app.ts
@@ -0,0 +1,3 @@
+let personName : string = "shehnila";
+console.log(`Hellow ${personName} would you like to learn some python today?`);
+
